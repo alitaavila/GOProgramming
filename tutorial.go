5e84aeb1460e630c9356cc18fde3f8319102c38e
Alejandra M. Avila <alejandra.avilaperez@gmail.com>
2022-05-01 10:43:48 -0600

    Update tutorial.go
    Updated

--- tutorial.go
+++ tutorial.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
+	// Hello world
 	fmt.Println("Hello World!")
+
+	/*
+		Variables: declarar tipo de la variable es una buena práctica, especialmente
+		cuando se quiere aprovechar el consumo de memoria (bits)
+	*/
+	var number int
+	number = 25
+	fmt.Println(number)
+	number = 45
+	fmt.Println(number)
+
 }
